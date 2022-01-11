bd3dad21a2e8c1fdd4a1939175f4e7893c8cf5cb
skywalker-am <mishra100abhi@gmail.com>
2022-01-11 18:35:53 +0530

    first change

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.get("/", function(req,res){
+    //res.send("Hey Guys");
+    res.render("list");
+});
+
+app.listen(3000,function(){
+    console.log("Listening on port 3000");
+})
