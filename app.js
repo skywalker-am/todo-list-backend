873f78dbf200e63e6e0a71862623e871cbf7394a
abhi.aing <abhijeet.goel@testaing.com>
2022-01-14 19:39:05 +0530

    port updated

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get("/", function(req,res){
     res.render("list");
 });
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Listening on port 3000");
 })
